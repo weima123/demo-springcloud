f53d7fcc0a1b8044f2ded782805def2abf5c9565
马卫 <wei.ma>
2020-03-16 11:52:59 +0800

    first commit

---
+++ hytrix/src/main/java/com/donlevy/springcloud/hytrix/HytrixApplication.java
@@ -0,0 +1,13 @@
+package com.donlevy.springcloud.hytrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HytrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HytrixApplication.class, args);
+    }
+
+}
---
+++ hytrix/src/test/java/com/donlevy/springcloud/hytrix/HytrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donlevy.springcloud.hytrix;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HytrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
